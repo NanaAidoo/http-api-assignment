a00751b71ae5aad5b0c0e8946931163fb1e985e8
NanaAidoo <boyslife288@gmail.com>
2020-09-15 19:08:26 -0400

    http-api-assignment

--- src/server.js
+++ src/server.js
@@ -7,34 +7,35 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
-  '/style.css': htmlHandler.getStyle,
-  '/success': responseHandler.success,
-  '/badRequest': responseHandler.badRequest,
-  '/unauthorized': responseHandler.unauthorized,
-  '/forbidden': responseHandler.forbidden,
-  '/internal': responseHandler.internal,
-  '/notImplemented': responseHandler.notImp,
-  notFound: responseHandler.notFound,
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getStyle,
+    '/success': responseHandler.success,
+    '/badRequest': responseHandler.badRequest,
+    '/unauthorized': responseHandler.unauthorized,
+    '/forbidden': responseHandler.forbidden,
+    '/internal': responseHandler.internal,
+    '/notImplemented': responseHandler.notImp,
+    notFound: responseHandler.notFound,
 };
 
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
-  const params = query.parse(parsedUrl.query);
-  const { valid } = query.parse(parsedUrl.query);
-    const { loggedIn } = query.parse(parsedUrl.query);
-  const acceptedTypes = request.headers.accept.split(',');
-  console.dir(parsedUrl);
-  console.dir(params);
-  console.dir(valid);
-  console.log(acceptedTypes);
+    const parsedUrl = url.parse(request.url);
+    const params = query.parse(parsedUrl.query);
+    const valid = query.parse(parsedUrl.query);
+    const loggedIn = query.parse(parsedUrl.query);
+    const acceptedTypes = request.headers.accept.split(',');
+    console.dir(parsedUrl);
+    console.dir(params);
+    console.dir(loggedIn);
+    console.dir(valid);
+    console.log(acceptedTypes);
 
-  const route = parsedUrl.pathname;
-  if (urlStruct[route]) {
-    urlStruct[route](request, response, acceptedTypes, params);
-  } else {
-    urlStruct.notFound(request, response, acceptedTypes, params);
-  }
+    const route = parsedUrl.pathname;
+    if (urlStruct[route]) {
+        urlStruct[route](request, response, acceptedTypes, params);
+    } else {
+        urlStruct.notFound(request, response, acceptedTypes, params);
+    }
 };
 
 http.createServer(onRequest).listen(port);
